af886fad1aeb3793f78d7b267409a4fd857910ed
otutukingsley <chineduotutu3@gmail.com>
2022-03-09 06:32:52 -0800

    logger saved

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { useEffect, Fragment } from 'react'
-import Searchbar from './components/layouts/Searchbar'
-import Logger from './components/logs/Logger'
-import AddBtn from './components/layouts/AddBtn'
-import AddLogModal from './components/logs/AddLogModal'
-import EditLogModal from './components/logs/EditLogModal'
-import AddTechModal from './components/techs/AddTechModal'
-import TechListModal from './components/techs/TechsListModal'
-import { Provider } from 'react-redux'
-import store from './store'
+import React, { useEffect, Fragment } from "react"
+import Searchbar from "./components/layouts/Searchbar"
+import Logger from "./components/logs/Logger"
+import AddBtn from "./components/layouts/AddBtn"
+import AddLogModal from "./components/logs/AddLogModal"
+import EditLogModal from "./components/logs/EditLogModal"
+import AddTechModal from "./components/techs/AddTechModal"
+import TechListModal from "./components/techs/TechsListModal"
+import { Provider } from "react-redux"
+import store from "./store"
 
-import 'materialize-css/dist/css/materialize.min.css'
-import M from 'materialize-css/dist/js/materialize.min.js'
-import './App.css'
+import "materialize-css/dist/css/materialize.min.css"
+import M from "materialize-css/dist/js/materialize.min.js"
+import "./App.css"
 
 const App = () => {
   useEffect(() => {
